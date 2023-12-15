27491ced9d3b062508fbe8c17e930f4a04f61373
jayrussell <jalex.russell@gmail.com>
2023-12-15 18:42:46 +1100

    evaluate files

---
+++ colloquial_sql/evaluate.py
@@ -0,0 +1,3 @@
+#run_application(). A function that will run app.py and print the output if ran successfully, otherwise will return False.
+
+#a function that will be able to run the run_application() 100 times, and store the results in a list
